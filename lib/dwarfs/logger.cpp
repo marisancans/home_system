5c17375bfe074224cbaa86e4ccb62e60265c8769
maris <marisancans@gmail.com>
2020-03-25 14:33:52 +0200

    Made logger thread safe

--- lib/dwarfs/logger.cpp
+++ lib/dwarfs/logger.cpp
@@ -8,12 +8,17 @@ namespace dw {
         if(!file_path.empty()){
                 if(!txt_file_p)
                     txt_file_p = std::make_unique<fs::TxtFile>(file_path);
+                mtx.lock();
                 txt_file_p->append(fmt);
+                mtx.unlock();
         };
 
         // Signal
-        if(signal_p)
+        if(signal_p){
+            mtx.lock();
             signal_p->emit(fmt);
+            mtx.unlock();
+        }
 
         // console
         std::cout << fmt << std::endl;
